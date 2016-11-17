cf7c671e04ba82b825238030a183cac7b7efdadd
maurosantos <santos.mauroantonio@gmail.com>
2016-11-17 15:23:25 -0300

    - Alterações com style e actibity_main mais enxuta.

--- app/src/main/java/br/com/maurosantos/android/posologia/MainActivity.java
+++ app/src/main/java/br/com/maurosantos/android/posologia/MainActivity.java
@@ -3,10 +3,7 @@ package br.com.maurosantos.android.posologia;
 import android.content.Intent;
 import android.database.SQLException;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -31,18 +28,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPessoa = (Button) findViewById(R.id.btnPessoa);
         btnMedicamento = (Button) findViewById(R.id.btnMedicamento);
         btnPosologia = (Button) findViewById(R.id.btnPosologia);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.btnMenu);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         btnPessoa.setOnClickListener(this);
         btnMedicamento.setOnClickListener(this);
